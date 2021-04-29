4d483547a059ff6476fe45f3a340d18efd8e2c7d
Henini Leo <leo.henini@etu.univ-lyon1.fr>
2021-04-29 14:17:58 +0200

    Change data structures of messages + beginning of thread detection

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
 let folders = new Array();
-let clearStorage = browser.storage.local.clear();
-clearStorage.then(onCleared, onError);
+//let clearStorage = browser.storage.local.clear();
+//clearStorage.then(onCleared, onError);
 async function createTab() {
     browser.tabs.create({url: "../html/page.html"}).then(res => {
         console.log("Création du nouvel onglet.");
@@ -11,19 +11,37 @@ browser.browserAction.onClicked.addListener(createTab);
 browser.menus.create({
     contexts: ["folder_pane"],
     title: "Add folder to Githund",
-    onclick(info) {
+    async onclick(info) {
         let folder = info.selectedFolder;
-        folders.push(folder);
-        browser.storage.local.set({
-            folders: folders
-        });
+        let foldersStorage = await browser.storage.local.get('folders');
+        let length = Object.keys(foldersStorage).length;  // Taille du storage    
+        if (length == 0) {
+            folders.push(folder);
+            browser.storage.local.set({
+                folders: folders
+            });
+        } else {
+            let inclu = false;
+            for (let f of foldersStorage.folders) {
+                if (JSON.stringify(f) == JSON.stringify(folder)) {
+                    inclu = true;
+                    break;
+                }
+            }
+            if (!inclu) {
+                folders.push(folder);
+                browser.storage.local.set({
+                    folders: folders
+                });
+            }
+        }
     } 
 });
-
+/*
 function onCleared() {
     console.log("OK");
 }
 
 function onError(e) {
     console.log(e);
-}
+}*/
--- js/page.js
+++ js/page.js
@@ -1,6 +1,8 @@
 let listFolders = new Array();
-let listMessages = new Array(); //Array of array of object with folder name
+let listMessages = new Object(); //Array of array of object with folder name
 let fromGit = new Array();
+let isEvent = new Object();
+let eventInfo = new Object();
 
 start();
 
@@ -9,25 +11,55 @@ async function start() {
     let foldersName = new Array();
     listFolders = getStorage.folders;
     if (listFolders !== undefined) {
-        for (var folder of listFolders) {
+        for (let folder of listFolders) {
             let messages = await listMessagesFromFolder(folder);
-            listMessages = listMessages.concat(messages);
+            listMessages[folder.name] = messages;
             foldersName.push({
                 folderName: folder.name
             });
         }
-        for (var msg of listMessages) {
-            let res = await getHeaderFromIdMessage(msg.id);
-            let header = res[0];
-            if (header.hasOwnProperty("x-github-recipient-address")) {
-                fromGit.push(msg);
+        console.log("ListMessages : ",listMessages);
+        for (let arr in listMessages) {
+            for (let msg of listMessages[arr]) {  
+                let res = await getHeaderFromIdMessage(msg.id);
+                let header = res[0];
+                if (header.hasOwnProperty("x-github-recipient-address")) {                
+                    let tabIssue = getIdEvent(msg);
+                    if (tabIssue != -1) {
+                        fromGit.push(msg);
+                        let idEvent = tabIssue[0];
+                        let repo = tabIssue[1];
+                        let event = tabIssue[2];
+                        if (isEvent[repo] == null) {
+                            let data = new Array();
+                            data.push({
+                                repo: repo,
+                                event: event,
+                                idEvent: idEvent,
+                                message: msg
+                            });
+                            isEvent[repo] = data                    
+                        } else {
+                            isEvent[repo].push({
+                                repo: repo,
+                                event: event,
+                                idEvent: idEvent,
+                                message: msg
+                            });
+                        }                                    
+                    } else {
+                        fromGit.push(msg);
+                    }
+                }
             }
+            
         }
         fillTemplate("template-listFolders", foldersName, "folders ul");
         fillTemplate("template-listMessages", fromGit, "preview ul");
         $('#preview ul li').click(async function (e) {
             let id = $(this).data().id;
             let fullMessage = await browser.messages.getFull(id);
+            console.log(fullMessage);
             let str;
             if (fullMessage.parts[0].body != null) {
                 str = fullMessage.parts[0].body;
@@ -36,6 +68,33 @@ async function start() {
                 
             }
             $('#messages p').html(str);
+        });
+        $('#btnThread').click(function (e) {
+            for (var msg of fromGit) {
+                let data = new Array();
+                Object.keys(isEvent).forEach(function(key) {
+                    isEvent[key].forEach((elem) => {                        
+                        if (elem.message.id == msg.id) {
+                            let obj = {
+                                repo: elem.repo,
+                                event: elem.event,
+                                idEvent: elem.idEvent
+                            };
+                            $(`#preview ul li[data-id=${msg.id}]`)[0].innerHTML += " / Thread";
+                            $(`#preview ul li[data-id=${msg.id}]`).off('click').on('click', function (e) {
+                                Object.keys(isEvent).forEach(function (k) {
+                                    isEvent[key].forEach((e) => {
+                                        if (e.repo == obj.repo && e.event == obj.event && e.idEvent == obj.idEvent) {                                            
+                                            data.push(msg);                                            
+                                        }
+                                    });
+                                });
+                                fillTemplate("template-listMessages", data, "preview ul li");
+                            });
+                        }
+                    })                    
+                });
+            }
         });
     } else {
         $('#folders').hide();
@@ -53,7 +112,6 @@ function fillTemplate(idTemplate, data, idElem) {
 }
 
 async function listMessagesFromFolder(folder) {
-    console.log(folder)
     let messages = new Array();
     let page = await browser.messages.list(folder);
     messages = page.messages;
@@ -74,4 +132,21 @@ async function getHeaderFromIdMessage(id) {
     return promise = new Promise((resolve, reject) => {
         resolve(tab);
     });
+}
+
+function getIdEvent(message) {
+    let header = message.headerMessageId;
+    let index = header.indexOf("@github.com");
+    header = header.split('/');
+    if (index > -1 && header[2] == 'issues') {
+        return [header[3], header[1], header[2]];     //Numéro & repo & event
+    } else if (index > -1 && header[2] == 'pull') {
+        let id = header[3];
+        if (id.length > 1) {
+            id = id.slice(0,1);
+        }
+        return [id, header[1], header[2]]; 
+    } else {
+        return -1;
+    }
 }
